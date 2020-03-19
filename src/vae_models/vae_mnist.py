4691f09675b3e8fb64711d70fb902c2ec4b1523f
Dmitrii <dmitrykharkovsky@gmail.com>
2020-03-19 23:13:51 +0800

    Revert "refact mnist vae"
    This reverts commit 88013a061f8575744ba25b9a7755abc262ef5925.

--- src/vae_models/vae_mnist.py
+++ src/vae_models/vae_mnist.py
@@ -8,8 +8,6 @@ latent vector from a Gaussian distribution with mean = 0 and std = 1.
 [1] Kingma, Diederik P., and Max Welling.
 "Auto-Encoding Variational Bayes."
 https://arxiv.org/abs/1312.6114
-
-code modified by Dmitrii based on the example in keras
 '''
 
 from __future__ import absolute_import
@@ -19,13 +17,33 @@ from __future__ import print_function
 from keras.layers import Lambda, Input, Dense
 from keras.models import Model
 from keras.datasets import mnist
-from keras.losses import binary_crossentropy
+from keras.losses import mse, binary_crossentropy
 from keras.utils import plot_model
 from keras import backend as K
 
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 import os
+
+
+# reparameterization trick
+# instead of sampling from Q(z|X), sample epsilon = N(0,I)
+# z = z_mean + sqrt(var) * epsilon
+def sampling(args):
+    """Reparameterization trick by sampling from an isotropic unit Gaussian.
+    # Arguments
+        args (tensor): mean and log of variance of Q(z|X)
+    # Returns
+        z (tensor): sampled latent vector
+    """
+
+    z_mean, z_log_var = args
+    batch = K.shape(z_mean)[0]
+    dim = K.int_shape(z_mean)[1]
+    # by default, random_normal has mean = 0 and std = 1.0
+    epsilon = K.random_normal(shape=(batch, dim))
+    return z_mean + K.exp(0.5 * z_log_var) * epsilon
 
 
 def plot_results(models,
@@ -72,7 +90,7 @@ def plot_results(models,
             x_decoded = decoder.predict(z_sample)
             digit = x_decoded[0].reshape(digit_size, digit_size)
             figure[i * digit_size: (i + 1) * digit_size,
-            j * digit_size: (j + 1) * digit_size] = digit
+                   j * digit_size: (j + 1) * digit_size] = digit
 
     plt.figure(figsize=(10, 10))
     start_range = digit_size // 2
@@ -89,125 +107,95 @@ def plot_results(models,
     plt.show()
 
 
-class MnistVariationalAutoEncoder(object):
+# MNIST dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    def __init__(self, latent_dim, original_dim=784, intermediate_dim=200):
-        self.latent_dim = latent_dim
-        self.intermediate_dim = intermediate_dim
-        self.original_dim = original_dim
+image_size = x_train.shape[1]
+original_dim = image_size * image_size
+x_train = np.reshape(x_train, [-1, original_dim])
+x_test = np.reshape(x_test, [-1, original_dim])
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
 
-        input_shape = (self.original_dim,)
+# network parameters
+input_shape = (original_dim, )
+intermediate_dim = 512
+batch_size = 128
+latent_dim = 2
+epochs = 50
 
-        # VAE model = encoder + decoder
+# VAE model = encoder + decoder
+# build encoder model
+inputs = Input(shape=input_shape, name='encoder_input')
+x = Dense(intermediate_dim, activation='relu')(inputs)
+z_mean = Dense(latent_dim, name='z_mean')(x)
+z_log_var = Dense(latent_dim, name='z_log_var')(x)
 
-        # build encoder model
-        inputs = Input(shape=input_shape, name='encoder_input')
-        x_interm = Dense(self.intermediate_dim, activation='relu')(inputs)
-        x = Dense(self.intermediate_dim, activation='relu')(x_interm)
-        z_mean = Dense(self.latent_dim, name='z_mean')(x)
-        z_log_var = Dense(self.latent_dim, name='z_log_var')(x)
+# use reparameterization trick to push the sampling out as input
+# note that "output_shape" isn't necessary with the TensorFlow backend
+z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
 
-        # use reparameterization trick to push the sampling out as input
-        # note that "output_shape" isn't necessary with the TensorFlow backend
-        z = Lambda(self.sampling, output_shape=(self.latent_dim,), name='z')([z_mean, z_log_var])
+# instantiate encoder model
+encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+encoder.summary()
+plot_model(encoder, to_file='vae_mlp_encoder.png', show_shapes=True)
 
-        # instantiate encoder model
-        encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
-        self.encoder = encoder
-        # encoder.summary()
-        plot_model(encoder, to_file='vae_mlp_encoder.png', show_shapes=True)
+# build decoder model
+latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
+x = Dense(intermediate_dim, activation='relu')(latent_inputs)
+outputs = Dense(original_dim, activation='sigmoid')(x)
 
-        # build decoder model
-        latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-        x_interm = Dense(self.intermediate_dim, activation='relu')(latent_inputs)
-        x = Dense(self.intermediate_dim, activation='relu')(x_interm)
-        outputs = Dense(self.original_dim, activation='sigmoid')(x)
+# instantiate decoder model
+decoder = Model(latent_inputs, outputs, name='decoder')
+decoder.summary()
+plot_model(decoder, to_file='vae_mlp_decoder.png', show_shapes=True)
 
-        # instantiate decoder model
-        decoder = Model(latent_inputs, outputs, name='decoder')
-        self.decoder = decoder
-        # decoder.summary()
-        # plot_model(decoder, to_file='vae_mlp_decoder.png', show_shapes=True)
-
-        # instantiate VAE model
-        outputs = decoder(encoder(inputs)[2])
-        vae = Model(inputs, outputs, name='vae_mlp')
-
-        reconstruction_loss = binary_crossentropy(inputs, outputs)
-
-        reconstruction_loss *= original_dim
-        kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
-        kl_loss = K.sum(kl_loss, axis=-1)
-        kl_loss *= -0.5
-        vae_loss = K.mean(reconstruction_loss + kl_loss)
-        vae.add_loss(vae_loss)
-        vae.compile(optimizer='adam')
-        self.vae = vae
-        # vae.summary()
-        # plot_model(vae,to_file='vae_mlp.png', show_shapes=True)
-
-    # reparameterization trick
-    # instead of sampling from Q(z|X), sample epsilon = N(0,I)
-    # z = z_mean + sqrt(var) * epsilon
-    @staticmethod
-    def sampling(args):
-        """Reparameterization trick by sampling from an isotropic unit Gaussian.
-        # Arguments
-
-            args (tensor): mean and log of variance of Q(z|X)
-        # Returns
-            z (tensor): sampled latent vector
-        """
-
-        z_mean, z_log_var = args
-        batch = K.shape(z_mean)[0]
-        dim = K.int_shape(z_mean)[1]
-        # by default, random_normal has mean = 0 and std = 1.0
-        epsilon = K.random_normal(shape=(batch, dim))
-        return z_mean + K.exp(0.5 * z_log_var) * epsilon
-
-    @staticmethod
-    def process_mnist_data():
-        # MNIST dataset
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-        image_size = x_train.shape[1]
-        original_dim = image_size * image_size
-        x_train = np.reshape(x_train, [-1, original_dim])
-        x_test = np.reshape(x_test, [-1, original_dim])
-        x_train = x_train.astype('float32') / 255
-        x_test = x_test.astype('float32') / 255
-        return x_train, x_test
-
-    def train(self, x_train=None, x_test=None, weights=None, batch_size=128, epochs=50):
-        if not x_train:
-            x_train, x_test = self.process_mnist_data()
-
-        if weights:
-            self.vae.load_weights(weights)
-        else:
-            self.vae.fit(x_train,
-                         epochs=epochs,
-                         batch_size=batch_size,
-                         validation_data=(x_test, None))
-            self.vae.save_weights('vae_mlp_mnist.h5')
-
+# instantiate VAE model
+outputs = decoder(encoder(inputs)[2])
+vae = Model(inputs, outputs, name='vae_mlp')
 
 if __name__ == '__main__':
-    latent_dim = 2
-    batch_size = 128
-    vae = MnistVariationalAutoEncoder(latent_dim=latent_dim)
-    vae.train()
-
-    _,  (x_test, y_test) = mnist.load_data()
-
-    image_size = x_test.shape[1]
-    original_dim = image_size * image_size
-    x_test = np.reshape(x_test, [-1, original_dim])
-    x_test = x_test.astype('float32') / 255
+    parser = argparse.ArgumentParser()
+    help_ = "Load h5 model trained weights"
+    parser.add_argument("-w", "--weights", help=help_)
+    help_ = "Use mse loss instead of binary cross entropy (default)"
+    parser.add_argument("-m",
+                        "--mse",
+                        help=help_, action='store_true')
+    args = parser.parse_args()
+    models = (encoder, decoder)
     data = (x_test, y_test)
 
-    plot_results((vae.encoder, vae.decoder),
+    # VAE loss = mse_loss or xent_loss + kl_loss
+    if args.mse:
+        reconstruction_loss = mse(inputs, outputs)
+    else:
+        reconstruction_loss = binary_crossentropy(inputs,
+                                                  outputs)
+
+    reconstruction_loss *= original_dim
+    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+    kl_loss = K.sum(kl_loss, axis=-1)
+    kl_loss *= -0.5
+    vae_loss = K.mean(reconstruction_loss + kl_loss)
+    vae.add_loss(vae_loss)
+    vae.compile(optimizer='adam')
+    vae.summary()
+    plot_model(vae,
+               to_file='vae_mlp.png',
+               show_shapes=True)
+
+    if args.weights:
+        vae.load_weights(args.weights)
+    else:
+        # train the autoencoder
+        vae.fit(x_train,
+                epochs=epochs,
+                batch_size=batch_size,
+                validation_data=(x_test, None))
+        vae.save_weights('vae_mlp_mnist.h5')
+
+    plot_results(models,
                  data,
                  batch_size=batch_size,
                  model_name="vae_mlp")
