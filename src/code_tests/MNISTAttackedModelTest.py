fe0a7deea572083241075fc1ba654d43fa4e8dd4
Dmitrii <dmitrykharkovsky@gmail.com>
2020-04-24 15:01:01 +0800

    added MNIST dataset loader

--- src/code_tests/MNISTAttackedModelTest.py
+++ src/code_tests/MNISTAttackedModelTest.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from attacked_models.AttackedModelFactory import AttackedModelFactory
 from dataset_data.mnist.MnistDescriptor import MNISTDescriptor
+from dataset_data.mnist.MnistLoader import MnistLoader
 from vae_models.VAEFactory import VAEFactory
 
 if __name__ == '__main__':
@@ -9,15 +10,19 @@ if __name__ == '__main__':
     mnist_root = '../../datasets/'
 
     dataset_descriptor = MNISTDescriptor()
-    vae = VAEFactory.get_vae(dataset_descriptor=dataset_descriptor,
-                             latent_dimension=latent_dim)
+    # vae = VAEFactory.get_vae(dataset_descriptor=dataset_descriptor,
+    #                          latent_dimension=latent_dim)
     # vae.train(num_epochs=50, dataset_folder=mnist_root)
 
-    x = np.atleast_2d(np.ones(latent_dim))
-    y = vae.decode(x)
-    print(y.shape)
+    # x = np.atleast_2d(np.ones(latent_dim))
+    # y = vae.decode(x)
+    # print(y.shape)
 
     dataset_descriptor.attacked_model_path = '../src/attacked_models/mnist/mnist'
-    attacked_model = AttackedModelFactory.get_attacked_model(dataset_descriptor)
-    predictions = attacked_model.predict(y)
-    print(predictions)
+    mnist_loader = MnistLoader()
+    dataset_descriptor.dataset_folder = '../../datasets/'
+    mnist_loader.load_data(dataset_folder=dataset_descriptor.dataset_folder)
+
+    # attacked_model = AttackedModelFactory.get_attacked_model(dataset_descriptor)
+    # predictions = attacked_model.predict(y)
+    # print(predictions)
---
+++ src/dataset_data/mnist/MnistLoader.py
@@ -0,0 +1,72 @@
+"""
+This code is modified by Dmitrii from the original code by Nicholas Carlini <nicholas@carlini.com>.
+"""
+
+import numpy as np
+import os
+import gzip
+import urllib.request
+
+
+class MnistLoader:
+    def __init__(self):
+        self.VALIDATION_SIZE = 5000
+
+        self.train_data = None
+        self.train_labels = None
+
+        self.validation_data = None
+        self.validation_labels = None
+
+        self.test_data = None
+        self.test_labels = None
+
+    def load_data(self, dataset_folder=None):
+        data_path = os.path.join(dataset_folder, "mnist_data")
+        if not os.path.exists(data_path):
+            print("Downloading MNIST dataset online")
+            self._download_mnist_data(data_path)
+        else:
+            print("Loading MNIST dataset from a local folder")
+
+        train_data = self._extract_inputs(f"{data_path}/train-images-idx3-ubyte.gz", 60000)
+        train_labels = self._extract_labels(f"{data_path}/train-labels-idx1-ubyte.gz", 60000)
+
+        self.test_data = self._extract_inputs(f"{data_path}/t10k-images-idx3-ubyte.gz", 10000)
+        self.test_labels = self._extract_labels(f"{data_path}/t10k-labels-idx1-ubyte.gz", 10000)
+
+        self.validation_data = train_data[:self.VALIDATION_SIZE, :, :, :]
+        self.validation_labels = train_labels[:self.VALIDATION_SIZE]
+
+        self.train_data = train_data[self.VALIDATION_SIZE:, :, :, :]
+        self.train_labels = train_labels[self.VALIDATION_SIZE:]
+
+    @staticmethod
+    def _download_mnist_data(data_path):
+        os.mkdir(data_path)
+        files = ["train-images-idx3-ubyte.gz",
+                 "t10k-images-idx3-ubyte.gz",
+                 "train-labels-idx1-ubyte.gz",
+                 "t10k-labels-idx1-ubyte.gz"]
+        for name in files:
+            urllib.request.urlretrieve('http://yann.lecun.com/exdb/mnist/' + name, f"{data_path}/" + name)
+
+    @staticmethod
+    def _extract_inputs(filename, num_images):
+        with gzip.open(filename) as bytestream:
+            bytestream.read(16)
+            buf = bytestream.read(num_images * 28 * 28)
+            data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+            # todo note this way of normalisation
+            data = (data / 255) - 0.5
+            data = data.reshape(num_images, 28, 28, 1)
+            return data
+
+    @staticmethod
+    def _extract_labels(filename, num_images):
+        with gzip.open(filename) as bytestream:
+            bytestream.read(8)
+            buf = bytestream.read(1 * num_images)
+            labels = np.frombuffer(buf, dtype=np.uint8)
+        num_classes = 10
+        return (np.arange(num_classes) == labels[:, None]).astype(np.float32)
