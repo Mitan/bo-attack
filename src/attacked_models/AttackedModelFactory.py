99f49248f62fd54acb5c0bc60a329be10e344f85
Dmitrii <dmitrykharkovsky@gmail.com>
2020-04-26 02:18:45 +0800

    use softmax not an optional parameter

--- src/attacked_models/AttackedModelFactory.py
+++ src/attacked_models/AttackedModelFactory.py
@@ -7,9 +7,10 @@ class AttackedModelFactory:
         pass
 
     @staticmethod
-    def get_attacked_model(dataset_descriptor):
+    def get_attacked_model(dataset_descriptor, use_softmax=True):
         if dataset_descriptor.dataset_type == DatasetEnum.MNIST:
-            model = MNISTAttackedModel(weight_load_path=dataset_descriptor.attacked_model_path)
+            model = MNISTAttackedModel(weight_load_path=dataset_descriptor.attacked_model_path,
+                                       use_softmax=use_softmax)
         else:
             raise NotImplementedError
         return model
